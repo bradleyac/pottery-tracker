a52fcb7615de3262d0ceaa672991107c451b1661
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-03-18 11:24:53 -0400

    Add vitest and storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import ts from 'typescript-eslint';
 import svelte from 'eslint-plugin-svelte';
@@ -5,19 +8,19 @@ import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 
 export default [
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...svelte.configs['flat/recommended'],
+    prettier,
+    ...svelte.configs['flat/prettier'],
+    {
 		languageOptions: { globals: { ...globals.browser, ...globals.node } }
 	},
-
-	{
+    {
 		files: ['**/*.svelte'],
 		languageOptions: { parserOptions: { parser: ts.parser } }
 	},
-	{ ignores: ['build/', '.svelte-kit/', 'dist/'] },
-	svelte.configs.prettier
+    { ignores: ['build/', '.svelte-kit/', 'dist/'] },
+    svelte.configs.prettier,
+    ...storybook.configs["flat/recommended"]
 ];
